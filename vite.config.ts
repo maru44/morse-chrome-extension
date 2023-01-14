89d8f4927588bbd7b4878b96532e679e87c8b12e
maru44 <takumaru22244@gmail.com>
2023-01-14 13:52:34 +0900

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import { crx, defineManifest } from '@crxjs/vite-plugin'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: 'Morse Input',
+  description: 'Input by morse code',
+  version: '1.0.0',
+  action: {
+    default_popup: 'index.html',
+  },
+})
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), crx({ manifest })],
 })
